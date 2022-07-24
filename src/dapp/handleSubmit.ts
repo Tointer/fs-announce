737de50bd1af4afba77f14d66fd50b7e37928a95
ibragimov.i <Tointer@yandex.ru>
2022-07-24 10:30:40 +0300

    add submit and get posts

---
+++ src/dapp/handleSubmit.ts
@@ -0,0 +1,23 @@
+import { create } from "ipfs-http-client";
+import  Lit  from "./litHelper"
+  
+export async function handleSubmit(contractAddress: string, message: string){
+    const client = create({ url: "https://ipfs.infura.io:5001/api/v0"});
+
+    try {
+        const created = await client.add(message);
+        const url = `https://ipfs.infura.io/ipfs/${created.path}`;
+
+        const encrypted = await Lit.encryptString(url, contractAddress);
+
+        console.log("ENCRYPTED:");
+        console.log(encrypted.encryptedFile);
+        console.log(encrypted.encryptedSymmetricKey);
+
+        return encrypted;
+    } catch (error) {
+        /* @ts-ignore */
+        console.log(error.message);
+    }
+
+}
