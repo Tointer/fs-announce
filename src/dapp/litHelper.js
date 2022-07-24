609bb8cf500b1c57b149d8ccf6f76cebfe24625f
ibragimov.i <Tointer@yandex.ru>
2022-07-24 10:48:18 +0300

    add helper

---
+++ src/dapp/litHelper.js
@@ -0,0 +1,76 @@
+//import * as LitJsSdk from "lit-js-sdk";
+import LitJsSdk from 'lit-js-sdk'
+
+const client = new LitJsSdk.LitNodeClient()
+const chain = 'ethereum'
+
+class Lit {
+  litNodeClient
+
+  async connect() {
+    await client.connect()
+    this.litNodeClient = client
+  }
+
+  getControlConditions(collectionAddress){
+    return [
+        {
+          contractAddress: collectionAddress,
+          standardContractType: 'ERC721',
+          chain,
+          method: 'balanceOf',
+          parameters: [
+            ':userAddress'
+          ],
+          returnValueTest: {
+            comparator: '>',
+            value: '0'
+          }
+        }
+      ]
+  }
+
+  async encryptString(str, collectionAddress) {
+    if (!this.litNodeClient) {
+      await this.connect()
+    }
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain })
+    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(str)
+
+    const encryptedSymmetricKey = await this.litNodeClient.saveEncryptionKey({
+      accessControlConditions: this.getControlConditions(collectionAddress),
+      symmetricKey,
+      authSig,
+      chain,
+    })
+
+    return {
+      encryptedFile: encryptedString,
+      encryptedSymmetricKey: LitJsSdk.uint8arrayToString(encryptedSymmetricKey, "base16")
+    }
+  }
+
+  async decryptString(encryptedStr, encryptedSymmetricKey, collectionAddress) {
+    if (!this.litNodeClient) {
+      await this.connect()
+    }
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain })
+    const symmetricKey = await this.litNodeClient.getEncryptionKey({
+      accessControlConditions: this.getControlConditions(collectionAddress),
+      toDecrypt: encryptedSymmetricKey,
+      chain,
+      authSig
+    })
+    const decryptedFile = await LitJsSdk.decryptString(
+      encryptedStr,
+      symmetricKey
+    );
+    // eslint-disable-next-line no-console
+    console.log({
+      decryptedFile
+    })
+    return { decryptedFile }
+  }
+}
+
+export default new Lit()
